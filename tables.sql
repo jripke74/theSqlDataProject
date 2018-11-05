09fff9cbb73c5fb22a5ded2d6c46992a1370eeb7
jripke74 <jripke74@gmail.com>
2018-11-04 20:06:11 -0800

    add cities to tables.sql

--- tables.sql
+++ tables.sql
@@ -4,22 +4,43 @@ CREATE TABLE country_tbl (
     PRIMARY KEY (country_id)
 );
 
-CREATE TABLE state_tbl (
-    state_id int NOT NULL,
-    state_name varchar(255),
-    PRIMARY KEY (state_id)
+CREATE TABLE state_provence_tbl (
+    state_provence_id int NOT NULL,
+    state_provence_name varchar(255),
+    country_id int,
+    PRIMARY KEY (state_id),
+    CONSTRAINT FK_country_tbl FOREIGN KEY (country_id)
+    REFERENCES country_tbl(country_id)
+);
+
+CREATE TABLE postal_code_tbl (
+    postal_code_id int NOT NULL,
+    postal_code varchar(255),
+    state_provence_id int,
+    PRIMARY KEY (county_id),
+    CONSTRAINT FK_state_provedence_tbl FOREIGN KEY (county_id)
+    REFERENCES state_provedence_tbl(state_id)
+);
+
+CREATE TABLE county_tbl (
+    county_id int NOT NULL,
+    county_name varchar(255),
+    state_provence_id int,
+    PRIMARY KEY (county_id),
+    CONSTRAINT FK_state_provedence_tbl FOREIGN KEY (state_id)
+    REFERENCES state_tbl(state_id)
 );
 
 CREATE TABLE city_tbl (
     city_id int NOT NULL,
     city_name varchar(255),
-    state_id int,
+    state_provence_id int,
     country_id int NOT NULL,
     PRIMARY KEY (city_id),
-    CONSTRAINT FK_state_tbl FOREIGN KEY (state_id)
-    REFERENCES state_tbl(state_id),
     CONSTRAINT FK_country_tbl FOREIGN KEY (country_id)
-    REFERENCES country_tbl(country_id)
+    REFERENCES country_tbl(country_id),
+    CONSTRAINT FK_state_provedence_tbl FOREIGN KEY (state_id)
+    REFERENCES state_tbl(state_id)
 );
 
 CREATE TABLE address_tbl  (
@@ -27,16 +48,19 @@ CREATE TABLE address_tbl  (
     address varchar(255),
     address_2 varchar(255),
     city_id int,
-    postal_code varchar(255),
-    state_id int,
+    postal_code_id int,
+    county_id int,
+    state_provence_id int,
     country_id int,
     PRIMARY KEY (address_id),
     CONSTRAINT FK_city FOREIGN KEY (city_id)
     REFERENCES city_tbl(city_id),
+    CONSTRAINT FK_postal_code_tbl FOREIGN KEY (postal_code_id)
+    REFERENCES postal_code_tbl(postal_code_id),
     CONSTRAINT FK_state FOREIGN KEY (state_id)
     REFERENCES state_tbl(state_id),
     CONSTRAINT FK_country FOREIGN KEY (country_id)
-    REFERENCES country_tbl(country_id) 
+    REFERENCES country_tbl(country_id)
 );
 
 CREATE TABLE person_tbl (
